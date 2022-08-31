a44b16681c9742bdaac819f2697b42ea63a13f18
Angela Gomez <angelagomezm@gmail.com>
2022-08-31 23:05:29 +0100

    Phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map((phonetic, index) => {
+          return (
+            <div>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map((meaning, index) => {
           //   return meaning.definitions[0].definition;
           return (
